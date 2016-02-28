1397d0439b97c84a5662e15ba2fc9ddf2720f051
VERMICULITE <seanowenhayes@gmail.com>
2016-02-28 13:36:23 +0000

    Added streamability

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 "use strict";
 
-function verifyArguments(page, spider, next) {
-    if (!page) {
-        throw new Error('css links requires a page.');
-    }
-    if (!page.$) {
-        throw new Error('css links requires a jquery like object on $ property');
-    }
-    if (!spider) {
-        throw new Error('css links requires a spider be passed.');
-    }
-    if (typeof  spider.addUrl !== 'function') {
-        throw new Error('css links requires to spider to have an addUrl function');
-    }
-    if (typeof next !== 'function') {
-        throw new Error('css-links requires a next function to be passed.');
-    }
-}
-
+let through2 = require('through2');
 
 module.exports = function (rule) {
-    return function (page, spider, next) {
-        verifyArguments(page, spider, next);
+    return through2.obj(function (page, next) {
+        let spider = page.spider;
         page.$(rule).each(function() {
             spider.addUrl(page.$(this).attr('href'));
         });
         next();
-    }
+    });
 };
--- test/index.js
+++ test/index.js
@@ -18,64 +18,33 @@ describe('mrspider-css-links', function() {
             addUrl: function() {}
         };
         validPage = {
-            $: cheerio.load('<h1><a class="test" href="http://abc.com/test">asd;k;ad ;aksd</a><a href="http://abc.com/test/1">asd;k;ad ;aksd</a><a href="http://abc.com/test/2">asd;k;ad ;aksd</a><a href="http://abc.com/test/3">asd;k;ad ;aksd</a></h1>')
+            $: cheerio.load('<h1><a class="test" href="http://abc.com/test">asd;k;ad ;aksd</a><a href="http://abc.com/test/1">asd;k;ad ;aksd</a><a href="http://abc.com/test/2">asd;k;ad ;aksd</a><a href="http://abc.com/test/3">asd;k;ad ;aksd</a></h1>'),
+            spider: validSpider
         }
         validCssLink = cssLinks(validRule);
     });
 
-    it('should throw an error given a non existant page', function() {
-        (function() {
-            validCssLink(null, validSpider, validNext);
-        }).should.throw(Error);
-    });
-
-    it('should throw an error given a page with no $ property', function() {
-        delete validPage.$;
-        (function() {
-            validCssLink(validPage, validSpider, validNext);
-        }).should.throw(Error);
-    });
-
-    it('should throw an error given no spider', function() {
-        (function() {
-            validCssLink(validPage, null, validNext);
-        }).should.throw(Error);
-    });
-
-    it('should throw an error given a spider with no addUrl function', function() {
-        delete validSpider.addUrl;
-        (function() {
-            validCssLink(validPage, validSpider, validNext);
-        }).should.throw(Error)
-    });
-
-    it('should throw an error given a nonexistant next', function() {
-        (function() {
-            validCssLink(validPage, validSpider, null);
-        }).should.throw(Error);
-    });
-
     it('should call addUrl of the spider given content with a link for the passed rule', function() {
-        validSpider.addUrl = sinon.spy();
-        validCssLink(validPage, validSpider, validNext);
-        validSpider.addUrl.calledOnce.should.equal(true);
+        validPage.spider.addUrl = sinon.spy();
+        validCssLink._transform(validPage, validNext);
+        validPage.spider.addUrl.calledOnce.should.equal(true);
     });
 
     it('should call addUrl of the spider given content with a link for the passed rule with the url of the href', function() {
-        validSpider.addUrl = sinon.spy();
-        validCssLink(validPage, validSpider, validNext);
-        validSpider.addUrl.calledOnce.should.equal(true);
+        validPage.spider.addUrl = sinon.spy();
+        validCssLink._transform(validPage, validNext);
+        validPage.spider.addUrl.calledOnce.should.equal(true);
         validSpider.addUrl.firstCall.args[0].should.equal('http://abc.com/test');
     });
 
     it('should call addUrl for each url matched givena rule that matches several links.', function() {
         var cssLink = cssLinks('h1 a');
-        validSpider.addUrl = sinon.spy();
-        cssLink(validPage, validSpider, validNext);
-        validSpider.addUrl.calledWith('http://abc.com/test').should.equal(true);
-        validSpider.addUrl.calledWith('http://abc.com/test/1').should.equal(true);
-        validSpider.addUrl.calledWith('http://abc.com/test/2').should.equal(true);
-        validSpider.addUrl.calledWith('http://abc.com/test/3').should.equal(true);
-        validSpider.addUrl.calledWith('http://abc.com/test/4').should.equal(false);
+        validPage.spider.addUrl = sinon.spy();
+        cssLink._transform(validPage, validNext);
+        validPage.spider.addUrl.calledWith('http://abc.com/test').should.equal(true);
+        validPage.spider.addUrl.calledWith('http://abc.com/test/1').should.equal(true);
+        validPage.spider.addUrl.calledWith('http://abc.com/test/2').should.equal(true);
+        validPage.spider.addUrl.calledWith('http://abc.com/test/3').should.equal(true);
+        validPage.spider.addUrl.calledWith('http://abc.com/test/4').should.equal(false);
     });
 });
